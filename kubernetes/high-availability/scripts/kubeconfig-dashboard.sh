f0e9aeef1911fcd024ef1868a145ed8663a635b4
Jhody Setiawan Sekardono <82011900+jhodysetiawansekardono@users.noreply.github.com>
2022-12-17 12:30:48 +0700

    Create kubeconfig-dashboard.sh

---
+++ kubernetes/high-availability/scripts/kubeconfig-dashboard.sh
@@ -0,0 +1,24 @@
+server=https://k8s-api.beruanglaut.cloud:8443
+name=admin-user
+ca=$(kubectl -n kubernetes-dashboard get secret/$name -o jsonpath='{.data.ca\.crt}')
+token=$(kubectl -n kubernetes-dashboard get secret/$name -o jsonpath='{.data.token}' | base64 --decode)
+
+echo "
+apiVersion: v1
+kind: Config
+clusters:
+- name: kubernetes
+  cluster:
+    certificate-authority-data: ${ca}
+    server: ${server}
+contexts:
+- name: admin@kubernetes
+  context:
+    cluster: kubernetes
+    user: admin
+current-context: admin@kubernetes
+users:
+- name: admin
+  user:
+    token: ${token}
+" > /tmp/kubeconfig-dashboard.yaml
