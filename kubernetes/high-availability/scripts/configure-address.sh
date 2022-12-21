f1749c7a3a31d162e35f72909426a2ddfab0c3fa
Jhody Setiawan Sekardono <82011900+jhodysetiawansekardono@users.noreply.github.com>
2022-12-21 14:02:56 +0700

    Update configure-address.sh

--- kubernetes/high-availability/scripts/configure-address.sh
+++ kubernetes/high-availability/scripts/configure-address.sh
@@ -3,6 +3,7 @@
 # IP Inventory
 K8S_VIP=10.15.0.10
 K8S_API=k8s-api.beruanglaut.cloud
+CLUSTER_NAME=beruanglaut-uat
 MASTER01=10.15.0.11
 MASTER02=10.15.0.12
 MASTER03=10.15.0.13
@@ -27,6 +28,11 @@ manifests/kubeadm/kubernetes-1.25.0.yaml \
 playbooks/containerd-1.25.0.yaml \
 playbooks/containerd-1.24.6.yaml \
 loadbalancer/check_apiserver.sh
+
+sed -i "s+CLUSTER_NAME+$CLUSTER_NAME+g" \
+manifests/kubeadm/kubernetes-1.15.9.yaml \
+manifests/kubeadm/kubernetes-1.24.6.yaml \
+manifests/kubeadm/kubernetes-1.25.0.yaml
 
 sed -i "s+MASTER01+$MASTER01+g" hosts.yaml
 sed -i "s+MASTER02+$MASTER02+g" hosts.yaml
