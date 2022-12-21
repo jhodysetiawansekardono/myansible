6335bc673ea843257decd21f5670533e08de92c8
Jhody Setiawan Sekardono <82011900+jhodysetiawansekardono@users.noreply.github.com>
2022-12-21 11:26:38 +0700

    Update check_apiserver.sh

--- kubernetes/high-availability/loadbalancer/check_apiserver.sh
+++ kubernetes/high-availability/loadbalancer/check_apiserver.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
-
-K8S_API=k8s-api.beruanglaut.cloud
-K8S_VIP=10.15.0.10
-
 errorExit() {
     echo "*** $*" 1>&2
     exit 1
 }
 
 curl --silent --max-time 2 --insecure https://localhost:6443/ -o /dev/null || errorExit "Error GET https://localhost:6443/"
-if ip addr | grep -q $K8S_VIP; then
-    curl --silent --max-time 2 --insecure https://$K8S_API:8443/ -o /dev/null || errorExit "Error GET https://$K8S_API:8443/"
+if ip addr | grep -q K8S_VIP; then
+    curl --silent --max-time 2 --insecure https://K8S_API:8443/ -o /dev/null || errorExit "Error GET https://K8S_API:8443/"
 fi
